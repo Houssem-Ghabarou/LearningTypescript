96fbde202f801631cc32201022c1252940f17f68
Houssem Ghabarou <44008549+Houssem-Ghabarou@users.noreply.github.com>
2024-01-18 23:50:04 +0100

    rest operator ts

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 "use strict";
-function showData(name, age, country) {
-    return `${name} ${age} ${country}`;
+function addAll(...nums) {
+    let result = 0;
+    for (let i = 0; i < nums.length; i++) {
+        result += nums[i];
+    }
+    return result;
 }
-console.log(showData("Jorge", 40, "country"));
+console.log(addAll(10, 20, 30, 10.5, +true));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-function showData(name?: string, age?: number, country?: string) {
-  return `${name} ${age} ${country}`;
+function addAll(...nums: number[]): number {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    result += nums[i];
+  }
+
+  return result;
 }
 
-console.log(showData("Jorge", 40, "country"));
+console.log(addAll(10, 20, 30, 10.5, +true));
