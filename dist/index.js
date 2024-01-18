1accac0c1ccf37bf4b1a6f7711fc594d0947330e
Houssem Ghabarou <44008549+Houssem-Ghabarou@users.noreply.github.com>
2024-01-19 00:57:21 +0100

    advanced enum types

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,23 @@
 "use strict";
-const EASY = 15;
-const MEDIUM = 10;
-const HARD = 5;
+function getHardSeconds() {
+    return 3;
+}
+var Kids;
+(function (Kids) {
+    Kids[Kids["Five"] = 25] = "Five";
+    Kids[Kids["Seven"] = 20] = "Seven";
+    Kids[Kids["Ten"] = 16] = "Ten";
+})(Kids || (Kids = {}));
 var level;
 (function (level) {
-    level[level["Easy"] = 15] = "Easy";
-    level[level["Medium"] = 10] = "Medium";
-    level[level["Hard"] = 5] = "Hard";
+    level[level["Insane"] = 0] = "Insane";
+    level[level["Kid"] = 16] = "Kid";
+    level[level["Easy"] = 9] = "Easy";
+    level[level["Medium"] = 6] = "Medium";
+    level[level["Hard"] = getHardSeconds()] = "Hard";
 })(level || (level = {}));
 let lvl = "Easy";
 if (lvl === "Easy") {
-    console.log(`The level is ${lvl} and the the number of second is ${level.Easy}`);
+    console.log(`The level is ${lvl} and the the number of second is ${level.Hard}`);
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 // enum type
 
-const EASY = 15;
-const MEDIUM = 10;
-const HARD = 5;
+function getHardSeconds(): number {
+  return 3;
+}
 
+enum Kids {
+  Five = 25,
+  Seven = 20,
+  Ten = 16,
+}
 enum level {
-  Easy = 15,
-  Medium = 10,
-  Hard = 5,
+  Insane,
+  Kid = Kids.Ten,
+  Easy = 9,
+  Medium = Easy - 3,
+  Hard = getHardSeconds(),
 }
 
 let lvl: string = "Easy";
 
 if (lvl === "Easy") {
   console.log(
-    `The level is ${lvl} and the the number of second is ${level.Easy}`
+    `The level is ${lvl} and the the number of second is ${level.Hard}`
   );
 }
